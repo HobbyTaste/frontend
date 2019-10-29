6b882417f151f7399f79f894394b413e40ba08ed
sikalov <sikalov@yandex-team.ru>
2019-10-29 23:47:00 +0300

    с собранным сервиром

--- build/app.js
+++ build/app.js
@@ -1,9 +1,40 @@
 "use strict";
-var express = require('express');
-var path = require('path');
-var withRoutes = require('./routes');
-var morgan = require('morgan');
-var app = express();
-app.use('/dist', express.static('dist'));
-app.use('/public/images', express.static('images'));
-app.listen(process.env.PORT || 3000);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var morgan_1 = __importDefault(require("morgan"));
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+var mongoose_1 = __importDefault(require("mongoose"));
+var logger_1 = __importDefault(require("./utils/logger"));
+var config = require('./config.json');
+var indexRoute = require('./routes/index');
+var hobby_1 = __importDefault(require("./routes/hobby"));
+var app = express_1.default();
+var LISTENING_PORT = process.env.PORT || Number(config.port) || 3000;
+var environment = process.env.NODE_ENV;
+app.use(express_1.default.json());
+app.use(morgan_1.default('dev'));
+app.use(express_1.default.urlencoded());
+app.use(cookie_parser_1.default());
+app.use('/dist', express_1.default.static('dist'));
+app.use('/public/images', express_1.default.static('images'));
+app.listen(LISTENING_PORT, function () {
+    logger_1.default.info("Server start listening on PORT: " + LISTENING_PORT + ", http://localhost:" + LISTENING_PORT);
+});
+// routes
+app.use(indexRoute);
+app.use('/hobby', hobby_1.default);
+app.use(function (err, req, res, next) {
+    res.status(404).end();
+});
+// MongoDB
+mongoose_1.default.connect(config.dbHost, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(function () {
+    logger_1.default.info("Connect to mongoDB: success");
+}, function (err) {
+    logger_1.default.error("MongoDB error: " + err);
+});
--- build/routes/index.js
+++ build/routes/index.js
@@ -1,10 +1,22 @@
 "use strict";
-var path = require('path');
-var fs = require('fs');
-module.exports = function (app) {
-    app.get('/', function (req, res) {
-        res.setHeader('Content-type', 'text/html');
-        res.send(fs.readFileSync(path.resolve(__dirname, '../../index.html')));
-    });
-    return app;
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = __importStar(require("express"));
+var router = express.Router();
+var TITLE = 'Hobby taste';
+function getHeader() {
+    return ("\n        <head>\n            <meta charset=\"UTF-8\">\n            <link href=\"public/images/favicon.ico\" rel=\"icon\">\n            <meta name=\"viewport\" content=\"width=768, initial-scale=1\">\n            <meta name=\"viewport\" content=\"width=425, initial-scale=1\">\n            <meta name=\"viewport\" content=\"width=375, initial-scale=1\">\n            <meta name=\"viewport\" content=\"width=320, initial-scale=1\">\n            <title>" + TITLE + "</title>\n        </head>\n        ");
+}
+function getBody() {
+    return ("\n         <body>\n            <div id=\"root\"></div>\n            <script src=\"dist/main.js\"></script>\n          </body>\n        ");
+}
+router.get('/', function (req, res) {
+    res.end("\n            <!DOCTYPE html>\n                <html lang=\"ru\">\n                    " + getHeader() + "\n                    " + getBody() + "\n                </html>\n            ");
+});
+module.exports = router;
