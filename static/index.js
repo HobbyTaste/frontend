96189badf376d28131593d38595527f4c97a57a6
sikalov <sikalov@yandex-team.ru>
2019-12-24 23:15:58 +0300

    выпиливание файлов react

--- static/index.js
+++ static/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import store from "./redux/store";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 
@@ -13,8 +11,3 @@ ReactDOM.render(
             <App/>
         </Provider>
     </BrowserRouter>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
