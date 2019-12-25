1648879193189e0508946db72fc67bd6b31c2822
sikalov <sikalov@yandex-team.ru>
2019-12-26 00:26:49 +0300

    изменят релизный скрипт

--- tasks/release.sh
+++ tasks/release.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# собираем проект
-NODE_ENV=production yarn buid
-
 # загружаем статику на s3
 aws --endpoint-url=https://storage.yandexcloud.net s3 sync dist/  s3://hobby-taste/static/
