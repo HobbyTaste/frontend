69f2a4001bffb985ae41bed5c4eb98b5b4e6dbe9
ignat <iggy99@mail.ru>
2020-03-19 17:31:07 +0300

    [HT-9] mongodb fixtures script

---
+++ server/fixtures/users.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+const ObjectId = require("mongodb").ObjectID;
+
+module.exports = [
+    {
+        _id: ObjectId("5e72c4a7e14c4b7594be81af"),
+        hobbies : ["5e72c4a7e14c4b7594be81ab"],
+        password : "$2b$10$TfdcS.R7gGFo1LlNO71jMeyxzrHhygnQAdVD0HjgkFCit29uvR/vS",
+        name : "Alice",
+        avatar : null,
+        email : "alice@test.com",
+        __v : 0
+    },
+    {
+        _id: ObjectId("5e72c4a7e14c4b7594be81b0"),
+        hobbies : ["5e72c4a7e14c4b7594be81ac", "5e72c4a7e14c4b7594be81ab"],
+        password : "$2b$10$.wwhvNhTtna1vhYaT6BQTucCIGwdm9iiPVZ/24vecSS71wM.bRo9C",
+        name : "Bob",
+        avatar : null,
+        email : "bob@test.com",
+        __v : 0
+    },
+    {
+        _id: ObjectId("5e72c4a7e14c4b7594be81b1"),
+        hobbies : ["5e72c4a7e14c4b7594be81ac"],
+        password : "$2b$10$l5AQ3PIRN4XoDcqQZ2kZOuji.p4Bto3zv2sPWZ0ubeVTEuzrJhPbG",
+        name : "Caroline",
+        avatar : null,
+        email : "caroline@test.com",
+        __v : 0
+    }
+];
