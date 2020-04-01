7cc2bc31432d9884b4df2f16cc25d16e9c6a61ad
yakun <ihatecapslook@gmail.com>
2020-04-01 18:16:04 +0300

    Добавленно создание kernel-а
    Кроме того:
    Добавлен файл destroy.sh
    Немного отредактированна инструкция

--- ml/enviroment/create.sh
+++ ml/enviroment/create.sh
@@ -3,3 +3,4 @@
 sudo apt install python3.6
 python3.6 -m venv env
 ./env/bin/pip install -r requirements.txt
+./env/bin/python -m ipykernel install --user --name python3-env-hobby-taste-ml --display-name "Hobby Taste ML"
