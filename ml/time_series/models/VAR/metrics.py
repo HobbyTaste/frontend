221c76fd4cd08b1e6de4bdfc1413bc1a395d4186
Denis Titov <titov.dyu@phystech.edu>
2020-04-03 23:28:12 +0300

    исправлены замечания по pep8, добавлен поиск парамтеров ARIMA

--- ml/time_series/models/VAR/metrics.py
+++ ml/time_series/models/VAR/metrics.py
@@ -1,11 +1,18 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
-def mean_absolute_percentage_error(y_true, y_pred): 
+
+def mean_absolute_percentage_error(
+    y_true: list,
+    y_pred: list
+) -> np.float:
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
-def root_mean_squared_error(y_true, y_pred):
+def root_mean_squared_error(
+    y_true: list,
+    y_pred: list
+) -> np.float:
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.sqrt(mean_squared_error(y_true, y_pred))
