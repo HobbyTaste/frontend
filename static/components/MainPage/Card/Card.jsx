1e95dc0ee2b2256d299bdfc702f5b0ea224e2ebd
DariaFil <dafi913@yandex.ru>
2020-04-11 21:13:45 +0300

    Исправлен внешний вид слайдера

--- static/components/MainPage/Card/Card.jsx
+++ static/components/MainPage/Card/Card.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import style from './Card.module.css';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import HalfRating from '../../HobbyCard/InformationForm/FeedbackStatistic';
+import color from '@material-ui/core/colors/red';
 
 const Card = (props) => {
     return (<div>
@@ -10,7 +11,7 @@ const Card = (props) => {
             <span className={style.cardDescription}>
                 <div className={style.cardHeader}>
                     <span className={`${style.name} ${style.colorWhiteCard}`}>{props.name}</span>
-                    <HalfRating isUserAuth = {props.isUserAuth}/>
+                    <span style={{ color: 'white' }}><HalfRating isUserAuth = {props.isUserAuth}/></span>
                 </div>
                 <div className={`${style.metro} ${style.colorBlueCard}`}>
                     <LocationOnIcon style={{ color: '#178FD6', fontSize: 'small' }} /> {props.metro}
