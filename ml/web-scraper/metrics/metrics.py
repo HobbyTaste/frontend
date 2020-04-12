a6e00c321f9c58ab5bc384b33c5b2d1d8c0e935e
yakun <ihatecapslook@gmail.com>
2020-04-12 05:14:45 +0300

    Все переделано под ООП
    Добавленны precision и recall

--- ml/web-scraper/metrics/metrics.py
+++ ml/web-scraper/metrics/metrics.py
@@ -1,39 +1,105 @@
 import numpy as np
 import pandas as pd
 
-def url_preproc(url: str):
-    # Убираем http в начале
-    url = url.replace('http://', '')
-    url = url.replace('https://', '')
+from typing import List, Dict, Set
+from copy import copy
+import math
 
-    # Убираем '/' в конце
-    if list(url)[-1] == '/':
-        url = "".join(list(url)[:-1])
+from preprocessor import Preprocessor
 
-    return url
-
-
-
-def calc_accuracy(data: pd.DataFrame, res: pd.DataFrame,
-                  col_in_data: str, col_in_res: str,
-                  key_in_data='Сайт', key_in_res='url',
-                  preproc=None, print_wrong_answers=False):
-
-    if preproc is None:
-        preproc = lambda x: x
+def isna(x):
+    return (x is None) \
+            or (x == 'None') \
+            or (x == 'nan') \
+            or (type(x) == float and math.isnan(x))
 
 
-    # Содержательная часть кода начинается тут
-    right_ans = 0
-    for i in res.index:
-        url = res[key_in_res][i]
-        prediction = res[col_in_res][i]
-        correct_answers = data[col_in_data][data[key_in_data] == url]
-        if np.any([preproc(prediction) == preproc(ans) for ans in correct_answers]):
-            right_ans += 1
-        elif print_wrong_answers:
-            print(f'Pred = {prediction}')
-            print(f'Correct = {correct_answers}')
-            print('')
+"""
+Recall - доля правильных ответов среди всех ответов парсера
+Precision - доля правильных ответов среди ненановских ответов парсера 
+В обоих случаях учитываются только те объекты, 
+у которых образцовый ответ - не njne
+"""
+class Metric:
+    def __init__(self, name: str, save_history=False):
+        assert name in {'precision', 'recall'}
+        self.name = name
+        self.save_history = save_history
+        if save_history:
+            self.history = []
 
-    return right_ans / len(res)
+    def _is_right(self, yp, yt: Set):
+        return yp in yt
+
+    def _is_wrong(self, yp, yt: Set):
+        if self.name == 'recall':
+            return not np.all([isna(y) for y in yt]) \
+                   and yp not in yt
+        elif self.name == 'precision':
+            return not np.any([isna(y) for y in yt]) \
+                   and yp not in yt \
+                   and not isna(yp)
+        else:
+            raise RuntimeError('Unknown metric type')
+
+    def calc(self, y_pred: List, y_true: List[Set])->float:
+        right, wrong = 0, 0
+
+        for yp, yt in zip(y_pred, y_true):
+            if self._is_right(yp, yt):
+                right += 1
+                loc_res = 'Right'
+            elif self._is_wrong(yp, yt):
+                wrong += 1
+                loc_res = 'Wrong'
+            else:
+                loc_res = 'Skipped'
+
+            if self.save_history:
+                self.history.append((loc_res, yp, yt))
+
+        if right + wrong == 0:
+            return np.nan
+        else:
+            return right / (right + wrong)
+
+
+def calc_metrics(data_pred: pd.DataFrame, data_true: pd.DataFrame,
+                 key: str, columns: List[str],
+                 metrics: List[str], preprocs: Dict[str, str],
+                 save_history=False):
+    data_pred = copy(data_pred)
+    data_true = copy(data_true)
+
+    def do_all_preprocs(data):
+        for col in preprocs:
+            cur_prep = Preprocessor(preprocs[col])
+            data[col] = data[col].apply(
+                lambda x: cur_prep.do_prep(x))
+
+    do_all_preprocs(data_pred)
+    do_all_preprocs(data_true)
+
+    result = pd.DataFrame(
+        data=np.zeros((len(metrics), len(columns))),
+        index=metrics, columns=columns,
+    )
+
+    if save_history:
+        history = {}
+
+    for col in columns:
+        y_pred = data_pred[col]
+        y_true = [set(data_true[data_true[key] == k][col])
+                  for k in data_pred[key]]
+
+        for metric_name in metrics:
+            metric = Metric(metric_name, save_history)
+            result[col][metric_name] = metric.calc(y_pred, y_true)
+            if save_history:
+                history[(col, metric_name)] = metric.history
+
+    if save_history:
+        return result, history
+    else:
+        return result
