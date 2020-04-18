c2bf6d01fe753823c9ef4a00b7a65297ec002c34
DariaFil <dafi913@yandex.ru>
2020-04-18 15:07:23 +0300

    Подключена секция отзывов (пока не специализированная)

--- static/components/UserCabinet/UserCabinet.jsx
+++ static/components/UserCabinet/UserCabinet.jsx
@@ -5,6 +5,7 @@ import style from './UserCabinet.module.css';
 import UserInfoCard from './UserInfoCard/UserInfoCard';
 import { initializeUserCabinet } from '../../redux/reducers/auth-reducer';
 import UserHobbyCard from './UserHobbyCard/UserHobbyCard';
+import Feedback from '../HobbyCard/Feedback/Feedback';
 
 const UserCabinet = (props) => {
     useEffect(() => {
@@ -20,6 +21,10 @@ const UserCabinet = (props) => {
         <div className={style.infoContainer}>
             <UserInfoCard avatar={props.avatar} name={props.name} metro={props.metro}/>
         </div>
+        <div className={style.feedbackHeader}>Ваши отзывы и ответы на них:</div>
+        <div className={style.feedbackContainer}>
+            <Feedback isUserAuth={props.isUserAuth} isProviderAuth={props.isProviderAuth} />
+        </div>
     </div>);
 };
 
@@ -31,7 +36,9 @@ const mapStateToProps = (state) => ({
     // avatar: state.auth.avatar,
     avatar: 'https://kravmaganewcastle.com.au/wp-content/uploads/2017/04/default-image-800x600.jpg',
     userHobbies: state.auth.userHobbies,
-    isAuth: state.auth.isAuth,
+    // isAuth: state.auth.isAuth,
+    isUserAuth: true,
+    isProviderAuth: false,
 });
 
 export default connect(mapStateToProps, { initializeUserCabinet })(UserCabinet);
