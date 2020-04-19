60777caf315ee1f8c9bb6327932560f8048c014a
yakun <ihatecapslook@gmail.com>
2020-04-19 12:11:31 +0300

    Убраны неработающие тесты

--- ml/web-scraper/tests/test_scraper.py
+++ ml/web-scraper/tests/test_scraper.py
@@ -19,15 +19,3 @@ def test_scraper_vk():
     assert res['address'] == 'Дорогомиловская Б. ул., д.5к2'
     assert res['metro_station'] == 'Киевская (линия 5)'
     assert res['image_source'] == 'https://sun9-51.userapi.com/c622920/v622920156/4ece1/ykksWy-fvBk.jpg?ava=1'
-
-def wrapped_scraper(urls: List[str], reject: List[str] = [])->pd.DataFrame:
-    data = pd.DataFrame(np.reshape(urls, (-1, 1)), columns=['Сайт'])
-    scraper = Scraper(data, reject, vk_token)
-    scraper.inspect()
-    return scraper.process_output()
-
-def test_scraper_some_page():
-    urls = ['https://kocherga-club.ru']
-    res = wrapped_scraper(urls)
-    print(res)
-    assert res == 42
