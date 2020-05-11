94ef26f27b6f64b7eca084eb3d028f0c323d0a91
Алексей <alexicon33@gmail.com>
2020-05-12 01:47:43 +0300

    Fixed comments, user cabinet and nginx

--- static/components/HobbyCard/Feedback/CommentText.js
+++ static/components/HobbyCard/Feedback/CommentText.js
@@ -1,85 +1,88 @@
-import  React, {Component} from 'react'
-import style from './Feedback.css';
-import Rating from '@material-ui/lab/Rating';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import ButtonProvider from '../Button/ButtonProvider';
-import { connect } from 'react-redux';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import CommentInput from './CommentInput';
-import { addProviderResponse, addUserFeedback } from '../../../redux/actions/hobbyActions';
+import React, { Component } from "react";
+import style from "./Feedback.css";
+import Rating from "@material-ui/lab/Rating";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ButtonProvider from "../Button/ButtonProvider";
+import { connect } from "react-redux";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import CommentInput from "./CommentInput";
+import { addProviderResponse, addUserFeedback } from "../../../redux/actions/hobbyActions";
 
-
-const useStyles = theme => ({
+const useStyles = (theme) => ({
     root: {
-        display: 'flex',
-        alignItems: 'center',
-        color: 'rgba(0, 0, 0, 0.54)',
+        display: "flex",
+        alignItems: "center",
+        color: "rgba(0, 0, 0, 0.54)",
     },
-    stars:{
-        color: 'rgba(0, 0, 0, 0.54)'
+    stars: {
+        color: "rgba(0, 0, 0, 0.54)",
     },
-    countStars:{
-        margin: '0px 5px',
-    }
+    countStars: {
+        margin: "0px 5px",
+    },
 });
-
 
 class CommentText extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isAnswered: false,
-            isOwner: props.isOwner,
-            isItAnswerProvider: props.isItAnswerProvider,
-            isProviderAuth: props.isProviderAuth,
-            text: props.comment.text,
-            nameWriter: props.comment.nameWriter,
-            date: props.comment.date,
-            stars: props.comment.stars,
-            isHaveAnswer: props.isHaveAnswer,
-
+            isAnswered: false
         };
         this.handleClick = this.handleClick.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleClick(event) {
-        this.setState({isAnswered: !this.state.isAnswered});
+        this.setState({ isAnswered: !this.state.isAnswered });
     }
-
 
     handleSubmit = (values) => {
         console.log(values);
-           this.props.onProviderResponse(this.props.hobbyId, values);
+        this.props.onProviderResponse(this.props.hobbyId, values);
     };
 
-    render(){
+    render() {
         const classes = useStyles();
         let classContainer = style.containerComment;
-        if (this.state.isItAnswerProvider) {
-            classContainer += (' ' + style.containerAnswer);
+        if (this.props.isItAnswerProvider) {
+            classContainer += " " + style.containerAnswer;
         }
         return (
             <div>
-            <div className={classContainer}>
-                <div className={style.info}>
-                    <div className={style.containerInfo}>
-                    <p className={style.icon}><AccountCircleIcon style={{ fontSize: 40}} /></p>
-                    <div className={style.infoData}>
-                        <p className={style.userName}>{this.state.nameWriter}</p>
-                        <p className={style.data}> {this.state.date}</p>
+                <div className={classContainer}>
+                    <div className={style.info}>
+                        <div className={style.containerInfo}>
+                            <p className={style.icon}>
+                                <AccountCircleIcon style={{ fontSize: 40 }} />
+                            </p>
+                            <div className={style.infoData}>
+                                <p className={style.userName}>{this.props.comment.nameWriter}</p>
+                                <p className={style.data}> {this.props.comment.date}</p>
+                            </div>
+                        </div>
+                        {this.props.isOwner && !this.props.isItAnswerProvider && !this.props.isHaveAnswer && (
+                            <ButtonProvider
+                                className={style.buttonContainer}
+                                onClick={this.handleClick}
+                                text="Ответить"
+                            />
+                        )}
                     </div>
+                    {!this.props.isItAnswerProvider && (
+                        <Rating
+                            style={classes.stars}
+                            size="small"
+                            name="half-rating-read"
+                            defaultValue={this.props.comment.stars}
+                            emptyIcon={<StarBorderIcon fontSize="inherit" />}
+                            precision={0.5}
+                            readOnly
+                        />
+                    )}
+                    <div className={style.text}>{this.props.comment.text}</div>
                 </div>
-                {(this.state.isOwner && !this.state.isItAnswerProvider && !this.state.isHaveAnswer) && <ButtonProvider className={style.buttonContainer} onClick={this.handleClick} text="Ответить"/>}
-                </div>
-        { !this.state.isItAnswerProvider && <Rating style={classes.stars} size='small' name="half-rating-read" defaultValue={this.state.stars} emptyIcon={<StarBorderIcon fontSize="inherit" />} precision={0.5} readOnly /> }
-        <div className={style.text}>
-            {this.state.text}
-        </div>
+                {this.props.isAnswered && <CommentInput onSubmit={this.handleSubmit} isAnswer={true} />}
             </div>
-                {this.state.isAnswered && <CommentInput onSubmit={this.props.handleSubmit} isAnswer={true}/>}
-            </div>
-
-);
+        );
     }
 }
 
@@ -88,5 +91,3 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(CommentText);
-
-
