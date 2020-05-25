d4e0b214681f807ec9ad788ffc919aef4885e657
Aleksey Ulyanov <alexicon33@gmail.com>
2020-05-25 16:34:39 +0300

    Added footer

--- static/components/Footer/Footer.jsx
+++ static/components/Footer/Footer.jsx
@@ -6,6 +6,15 @@ import Feedback from "./Feedback/Feedback";
 const Footer = (props) => {
     return (
         <footer className={style.footer}>
+            <div className={style.contacts}>
+                Студенческий проект “HobbyTaste”.<br/>
+                Инновационный практикум, весна 2020.
+            </div>
+            <div className={style.contacts}>
+                Наши контакты: <br/>
+                Руководитель проекта: Сикалов Никита <br/>
+                Email: sikalov.ns@phystech.edu
+            </div>
         </footer>
     );
 };
